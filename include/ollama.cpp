f3518add8c65b5e3f7d04800207b5b1ad579799d
gunderbus <cole.gundermann@gmail.com>
2026-05-02 18:57:58 -0400

    started the code

---
+++ include/ollama.cpp
@@ -0,0 +1,14 @@
+#include <ollama.h>
+
+ollama(std::string modelName){
+    // method stuff here
+}
+
+// Method to "ask" the AI
+void prompt(std::string userInput){
+    // method stuff here
+}
+    
+bool getIsTrue(std::string isTrue){
+    // method stuff here
+}
