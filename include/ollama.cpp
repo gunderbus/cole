0e4e651c6b15e98331b7989d24a799c98fb798ef
gunderbus <cole.gundermann@gmail.com>
2026-05-02 18:59:59 -0400

    im a idiot

--- include/ollama.cpp
+++ include/ollama.cpp
@@ -1,14 +1,15 @@
-#include <ollama.h>
+#include "ollama.h"
 
-ollama(std::string modelName){
+ollama::ollama(std::string modelName) : model(modelName) {
     // method stuff here
 }
 
 // Method to "ask" the AI
-void prompt(std::string userInput){
+void ollama::prompt(std::string userInput) {
     // method stuff here
 }
     
-bool getIsTrue(std::string isTrue){
+bool ollama::getIsTrue(std::string isTrue) {
     // method stuff here
+    return false;
 }
