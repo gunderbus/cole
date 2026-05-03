268f0575c9a597f225d88c17fa3e25bd44e19bb6
gunderbus <cole.gundermann@gmail.com>
2026-05-02 20:36:17 -0400

    chill bruh

--- include/person/person.h
+++ include/person/person.h
@@ -2,16 +2,22 @@
 #define PERSON_H
 
 #include <string>
+#include "nodes/nodes.h"
 
 class person {
 public:
     // Constructor
     person(std::string name, std::string contextPath);
 
-};
+    std::string getNeededPrompt(std::string Input);
+    std::string getContextPath();
+    std::string getConditionalPath(std:string Input);
 
-// private:
-//     std::string name;
-// };
+private:
+    
+    std::vector<node> connections;
+    str::string contextPath;
+
+};
 
 #endif
